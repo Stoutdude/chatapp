e868c4e84e8538a6fec0dc61b96f655ed328c049
John Webb <jmw@John's MacBook Pro>
2016-09-20 18:10:29 -0700

    Set view engine to jade and added title variable

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express = require("express");
 var app = express();
 
+app.set("views", "./views");
+app.set('view engine', 'jade');
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 
 app.get('/', function(req, res){
-  res.render("index.jade");
+  res.render("index", { title: "Home" });
 });
 
 app.get('/admin/rooms', function(req, res){
-  res.render("rooms.jade");
+  res.render("rooms", { title: "Admin Rooms" });
 });
 
 app.listen(3000, function() {
