23d854fe646ddc0210988e290616643e17174ae0
John Webb <jmw@John's MacBook Pro>
2016-09-23 11:16:16 -0700

    Added an express message service api and a jquery client to display messages

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'jade');
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
+app.use(express.static("node_modules/jquery/dist"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(function (req, res, next) {
@@ -15,12 +16,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // });
 
 app.get('/', function (req, res) {
-  res.render("index", { title: "Home" });
+  res.render("home", { title: "Home" });
 });
 
 var adminRouter = require("./admin");
 app.use("/admin", adminRouter);
 
+var apiRouter = require("./api");
+app.use("/api", apiRouter);
 
 app.listen(3000, function () {
   console.log('Chat app is listening on port 3000!');
