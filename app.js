62e9defe6df195832a9736ca2adaa11527705990
John Webb <jmw@John's MacBook Pro>
2016-09-23 16:01:25 -0700

    Added login page and passport local authentication strategy

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var passport = require("passport");
+require("./passport-init");
 
 app.set("views", "./views");
 app.set('view engine', 'jade');
 
-var fs = require("fs");
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'} )
-
-app.use(require("morgan")("combined", {stream: accessLogStream}));
+app.use(require("./logging.js"));
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
@@ -16,12 +15,23 @@ app.use(express.static("node_modules/jquery/dist"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(require('express-session')({
+  secret: 'keyboard cat', resave: false, saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // require('express-debug')(app, {});
 
 // app.use(function (req, res, next) {
 //   console.log(`Request: ${req.url}`);
 //   next();
 // });
+
+var authRouter = require("./auth");
+app.use(authRouter);
+
 
 app.get('/', function (req, res) {
   res.render("home", { title: "Home" });
