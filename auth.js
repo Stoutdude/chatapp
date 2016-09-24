fe575e24cf444991eb8f65550acee99a2de48b28
John Webb <jmw@John's MacBook Pro>
2016-09-24 11:07:23 -0700

    Added hard coded user login into the development environment

--- auth.js
+++ auth.js
@@ -1,10 +1,19 @@
 var express = require("express");
 var passport = require("passport");
+var users = require("./data/users.json");
 
 var router = express.Router();
 module.exports = router;
 
 router.get("/login", function (req, res) {
+    if (res.app.get("env") === "development") {
+        var user = users[0];
+        req.logIn(user, function (err) {
+            if (err) { return next(err); }
+            return res.redirect("/");
+        });
+        return;
+    }
     res.render("login");
 });
 
